46df4abf8d053039f836669a021dbd4276f9178e
Marcos Vieira <marcosvieira@labadmins-MacBook-Pro.local>
2017-05-31 15:42:09 -0500

    Adjusted file paths in shell script for running Partis

--- analyses/clonal_assignment/VRC01L_annotation.sh
+++ analyses/clonal_assignment/VRC01L_annotation.sh
@@ -2,4 +2,4 @@
 
 cd /partis
 
-./bin/partis run-viterbi --n-procs 4 --infname /host/Users/marcosvieira/Projects/evolution_evolvability/Data/Wu_2015_VRC01/Wu_2015_VRC01_light_curated.fasta --outfname /host/Users/marcosvieira/Projects/evolution_evolvability/Results/0_Clone_assignment/VRC01L_annotation.csv
+./bin/partis run-viterbi --n-procs 4 --infname /host/Users/marcosvieira/Projects/evolution_evolvability/Data/Wu_2015_VRC01/Wu_2015_VRC01_heavy_curated.fasta --outfname ~/Projects/evolution_of_mutability/results/clonal_assignment/VRC01/VRC01L_annotation.csv
