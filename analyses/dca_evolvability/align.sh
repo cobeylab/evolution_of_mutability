47dfadef8994f146ac7530b297bb70f8d0e0ca8f
GeroyUsilov <christopherjoel.russo@gmail.com>
2022-09-09 14:31:20 +0200

    refactored code and modified pipeline structure

---
+++ analyses/dca_evolvability/align.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hmmalign -o postprocessed_alignments/concat_aligned ../../data/ig_germline_alignments/vset.hmm preprocessed_alignments/concat.fasta
---
+++ analyses/dca_evolvability/lbsdc_script.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+../../../ig_dca/lbsDCA -f postprocessed_alignments/IGSF_excluded.fasta -t 4 -wid 0.9 -full -lh 0.2 -lj 0.3 -o model_outputs/model_outputs
